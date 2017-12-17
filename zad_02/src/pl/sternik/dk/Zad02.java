32d620eb50a3cc4e0edde1d5da2fc21539f0c6cb
KarwatDominika <dominika.karwat@edu.uekat.pl>
2017-12-17 09:54:47 +0100

    zad02

---
+++ zad_02/src/pl/sternik/dk/Zad02.java
@@ -0,0 +1,8 @@
+package pl.sternik.dk;
+
+public class Zad02 {
+    public static void main(String[] args) {
+        System.out.println("linia 1");
+        System.out.println("linia 2");
+    }
+}
